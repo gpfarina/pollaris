15f40fdeb235d28a2e3c91514ec59dd57a7ca770
Gian Pietro Farina <farinagianpietro@gmail.com>
2025-06-15 14:07:10 +0200

    comment and config

--- src/main/java/com/pollaris/config/BackendConfigEntry.java
+++ src/main/java/com/pollaris/config/BackendConfigEntry.java
@@ -4,6 +4,8 @@ package com.pollaris.config;
 
 /**
  * This calss contains basic information of a backend. Fow now only S3 and Local are supported.
+ * This class could probably handle also the authentication credentials for file systems such as s3.
+ * For now this part is left unimplemented.
  */
 public class BackendConfigEntry {
     public BackendType type;
